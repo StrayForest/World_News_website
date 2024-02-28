59fe1d31df8871df91604a04f404b96305184fb3
Aleksei Lisitcin <157023683+StrayForest@users.noreply.github.com>
2024-02-28 10:54:51 +0200

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -1,5 +1,9 @@
 from pathlib import Path
 import sys
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Определение MIDDLEWARE
 MIDDLEWARE = [
@@ -32,13 +36,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2aqnzv@j=4!n(uv74$j5xizqqth)1wm-ooq^idj@!8vm!t&66v'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ec2-35-156-75-243.eu-central-1.compute.amazonaws.com']
 
 
 # Application definition
